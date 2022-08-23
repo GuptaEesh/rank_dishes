cae6bfded94617fc18ac2be2a9dbc767c32d1008
GuptaEesh <p1r8a5t7ibha@gmail.com>
2022-08-23 14:37:32 +0530

    [feat] auth validations added

--- src/helpers/user-context.jsx
+++ src/helpers/user-context.jsx
@@ -6,22 +6,28 @@ const initialUserState = {
   users: [],
   loggedUser: {},
   authStatus: false,
+  usernameError: false,
+  passwordError: false,
 };
 const userReducer = (usersState, action) => {
   const { type, payload } = action;
-  const checkIfUserPresent = () =>
-    usersState.users.some(
-      (user) =>
-        user.username === payload.username && user.password === payload.password
-    );
+  const isUsernameCorrect = () =>
+    usersState.users.some((user) => user.username === payload.username);
+  const isPasswordCorrect = () =>
+    usersState.users.some((user) => user.password === payload.password);
+  const checkIfUserPresent = () => {
+    return isUsernameCorrect() && isPasswordCorrect();
+  };
   switch (type) {
     case ACTIONS.GET_ALL_USERS:
       return { ...usersState, users: payload };
     case ACTIONS.USER_CRED:
       return {
         ...usersState,
-        loggedUser: payload,
+        loggedUser: payload.userCreds,
         authStatus: checkIfUserPresent(),
+        usernameError: payload.username.length !== 0 && !isUsernameCorrect(),
+        passwordError: payload.password.length !== 0 && !isPasswordCorrect(),
       };
     default:
       break;
--- src/screens/authenticate-screen.jsx
+++ src/screens/authenticate-screen.jsx
@@ -4,45 +4,65 @@ import { useUser } from "../helpers/user-context";
 import { ACTIONS } from "../utils";
 import "./screens.css";
 const AuthenticateScreen = () => {
-  const { dispatchUser } = useUser();
+  const {
+    usersState: { usernameError, passwordError },
+    dispatchUser,
+  } = useUser();
   const [userCreds, setUserCreds] = useState({});
+  const [error, setError] = useState(false);
   const { username, password } = userCreds;
+
   const submitHandler = (e) => {
     e.preventDefault();
-    dispatchUser({ type: ACTIONS.USER_CRED, payload: userCreds });
-    setUserCreds({ username: "", password: "" });
+    setError(true);
+    dispatchUser({
+      type: ACTIONS.USER_CRED,
+      payload: userCreds,
+    });
   };
   const inputHandler = (e) => {
+    setError(false);
     setUserCreds((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
   const setExistingUserCreds = (e) => {
     e.preventDefault();
     setUserCreds({ username: "antony", password: "antony123" });
   };
+
   return (
     <article className="h-full flex align-center justify-center">
       <form
         onSubmit={submitHandler}
         className="flex flex-col g-1 justify-center login-form p-1 r-05"
       >
-        <InputSimple
-          title="UserName"
-          inputName="username"
-          inputClass="p-010 r-05"
-          inputPlaceholder="username.."
-          inputType="text"
-          inputValue={username ?? ""}
-          inputFunc={inputHandler}
-        />
-        <InputPass
-          inputName="password"
-          title="Password"
-          inputClass="p-010 r-05"
-          inputPlaceHolder="password.."
-          inputType="password"
-          inputValue={password ?? ""}
-          inputFunc={inputHandler}
-        />
+        <section>
+          <InputSimple
+            title="UserName"
+            inputName="username"
+            inputClass="p-010 r-05"
+            inputPlaceholder="username.."
+            inputType="text"
+            inputValue={username ?? ""}
+            inputFunc={inputHandler}
+          />
+          {error && usernameError ? (
+            <small className="error">Incorrect Username!</small>
+          ) : null}
+        </section>
+        <section>
+          <InputPass
+            inputName="password"
+            title="Password"
+            inputClass="p-010 r-05"
+            inputPlaceHolder="password.."
+            inputType="password"
+            inputValue={password ?? ""}
+            inputFunc={inputHandler}
+          />
+          {error && passwordError ? (
+            <small className="error">Incorrect Password!</small>
+          ) : null}
+        </section>
         <button
           className="cta-btn cursor-ptr r-05"
           onClick={setExistingUserCreds}
